3833b6c47c5bd81998b5d663c40cc3b46b54d31c
Barış SAVAŞ <barissavas17@gmail.com>
2023-08-29 22:35:32 +0300

    solved

---
+++ 21. Merge Two Sorted Lists.go
@@ -0,0 +1,62 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func CreateLinkedList(nums []int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	head := &ListNode{Val: nums[0]}
+	current := head
+
+	for i := 1; i < len(nums); i++ {
+		node := &ListNode{Val: nums[i]}
+		current.Next = node
+		current = node
+	}
+
+	return head
+}
+
+func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	current := dummy
+
+	for list1 != nil && list2 != nil {
+		if list1.Val < list2.Val {
+			current.Next = list1
+			list1 = list1.Next
+		} else {
+			current.Next = list2
+			list2 = list2.Next
+		}
+		current = current.Next
+	}
+
+	if list1 != nil {
+		current.Next = list1
+	} else {
+		current.Next = list2
+	}
+
+	return dummy.Next
+}
+
+func MergeTwoLists2(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+	if l1.Val < l2.Val {
+		l1.Next = MergeTwoLists2(l1.Next, l2)
+		return l1
+	}
+	l2.Next = MergeTwoLists2(l1, l2.Next)
+	return l2
+}
