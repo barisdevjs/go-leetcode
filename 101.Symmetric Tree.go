93a6d8b2adcdecb0d37568b71f6dc8d99aa319a7
Barış SAVAŞ <barissavas17@gmail.com>
2023-09-23 13:15:42 +0300

    solved

---
+++ 101.Symmetric Tree.go
@@ -0,0 +1,18 @@
+package main
+
+func IsSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return isMirror(root.Left, root.Right)
+}
+
+func isMirror(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && isMirror(p.Left, q.Right) && isMirror(p.Right, q.Left)
+}
